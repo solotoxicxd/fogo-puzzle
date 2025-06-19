6d24d910ac2c4e9572a3d15b671293cdcb20bb86
solotoxicxd <solotoxicxd@gmail.com>
2025-06-19 18:29:41 +0530

    Delete fogo-puzzle directory

--- fogo-puzzle/src/fetchScore.js
+++
@@ -1,8 +0,0 @@
-import { collection, getDocs, orderBy, query, limit } from "firebase/firestore";
-import { db } from "./firebase";
-
-export async function fetchTopScores() {
-  const q = query(collection(db, "scores"), orderBy("score", "desc"), limit(10));
-  const snapshot = await getDocs(q);
-  return snapshot.docs.map(doc => doc.data());
-}
