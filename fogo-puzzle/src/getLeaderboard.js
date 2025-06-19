c981c9dcc0f44c4617a53541aa59385e1ad6fba2
solotoxicxd <solotoxicxd@gmail.com>
2025-06-19 15:05:45 +0530

    Delete fogo-puzzle/src/getLeaderboard.js

--- fogo-puzzle/src/getLeaderboard.js
+++
@@ -1,7 +0,0 @@
-import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
-import { db } from "./firebase";
-
-export async function getTopScores() {
-  const q = query(collection(db, "scores"), orderBy("time"), limit(10));
-  const snapshot = await getDocs(q);
-  return snapshot.docs.map(doc => doc.data());
